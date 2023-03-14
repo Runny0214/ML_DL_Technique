f2416fc2981795523171b5d6bbb3a88dc62c45a0
Social_Datascientist <80202245+Runny0214@users.noreply.github.com>
2023-03-14 23:19:29 +0900

    Update directory.py

--- directory.py
+++ directory.py
@@ -1,6 +1,7 @@
 # directory 변경 후 csv file read
 # import os
 # os.getcwd() : 현재 directory 위치 확인
-# os.chdir() : 이동할 directory
+# os.chdir() : 이동할 directory 이동
+# -- 내 드라이브 경로에서 스페이스바 때문에 read가 안되는 오류 생김.
 # dirctory 변경 후, pd.read_csv() 통해 csv file reading
 
