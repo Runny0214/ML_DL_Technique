7d3d68926ff3dfc0badc6ad20b9f635b304aa540
Social_Datascientist <80202245+Runny0214@users.noreply.github.com>
2023-03-14 23:14:51 +0900

    Update directory.py

--- directory.py
+++ directory.py
@@ -1,3 +1,6 @@
-# Store Sales Prediction Kaggle Learning
-# 주기성 Time Series Data
-# 
+# directory 변경 후 csv file read
+# import os
+# os.getcwd() : 현재 directory 위치 확인
+# os.chdir() : 이동할 directory
+# dirctory 변경 후, pd.read_csv() 통해 csv file reading
+
